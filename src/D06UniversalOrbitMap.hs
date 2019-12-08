b76f23b71340fe50c1fb914ab7505c97229277eb
Sherub Thakur <sherub.thakur@gmail.com>
2019-12-08 19:56:38 +0530

    Not so inefficeint solution for part 2 of day 6

--- src/D06UniversalOrbitMap.hs
+++ src/D06UniversalOrbitMap.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE TupleSections #-}
 module D06UniversalOrbitMap where
 
+import Control.Arrow ((&&&))
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.List (nub)
 import Data.List.Split (splitOn)
 import Data.Tuple (swap)
-import Data.Maybe (fromMaybe)
-
+import Data.Maybe (fromMaybe, listToMaybe)
 
 type Node = String
 type Children = [String]
@@ -48,10 +48,32 @@ totalOrbits g = S.size $ allOrbits g (head $ roots g)
 
 -- Part II
 
-fromEdges' :: [(Node, Node)] -> Graph
-fromEdges' xs = M.fromListWith (++) . map (fmap (:[])) $ xs ++ map swap xs
+pathFromCenter :: Node -> Graph -> Node -> [Node]
+pathFromCenter searchNode g presentNode =
+    let
+        children = fromMaybe [] $ g M.!? presentNode
+        recPaths = map (pathFromCenter searchNode g) children
+        validPaths = filter (not . null) recPaths
+        maybeValidPath = listToMaybe validPaths
+        pathToReturn = case maybeValidPath of
+                            Nothing -> []
+                            Just path -> presentNode : path
+    in
+        if searchNode `elem` children then [presentNode, searchNode] else pathToReturn
+
+
+jumpsRequired :: Graph -> Int
+jumpsRequired g =
+    let
+        pathToYou = pathFromCenter "YOU" g "COM"
+        pathToSanta = pathFromCenter "SAN" g "COM"
+        you = S.fromList pathToYou 
+        san = S.fromList pathToSanta
+        uniqueJumps = (san `S.union` you) `S.difference` (san `S.intersection` you)
+    in
+        (S.size uniqueJumps) - 2
 
 
 main :: IO ()
 main = getContents >>=
-    print . totalOrbits . fromString
+    print . (totalOrbits &&& jumpsRequired) . fromString
--- test/D06UniversalOrbitMapTest.hs
+++ test/D06UniversalOrbitMapTest.hs
@@ -2,7 +2,7 @@ module D06UniversalOrbitMapTest where
 
 import Test.HUnit hiding (Node)
 
-import D06UniversalOrbitMap (Graph, fromEdges, totalOrbits, allOrbits)
+import D06UniversalOrbitMap (Graph, fromEdges, totalOrbits, allOrbits, jumpsRequired, pathFromCenter)
 import qualified Data.Set as S
 
 
@@ -22,10 +22,40 @@ g = fromEdges
     ]
 
 
+g1 :: Graph
+g1 = fromEdges
+    [ ("COM","B")
+    , ("B","C")
+    , ("C","D")
+    , ("D","E")
+    , ("E","F")
+    , ("B","G")
+    , ("G","H")
+    , ("D","I")
+    , ("E","J")
+    , ("J","K")
+    , ("K","L")
+    , ("K", "YOU")
+    , ("I", "SAN")
+    ]
+
+
 testTotalOrbits =
     [ TestLabel "" $ TestCase $ assertEqual "" 42 (totalOrbits g)
     ]
 
 
+testPathFromCenter =
+    [ TestLabel "" $ TestCase $ assertEqual "" ["COM", "B", "C", "D", "I", "SAN"] (pathFromCenter "SAN" g1 "COM")
+    , TestLabel "" $ TestCase $ assertEqual "" ["COM", "B", "C", "D", "E", "J", "K", "YOU"] (pathFromCenter "YOU" g1 "COM")
+    ]
+
+
+testJumpsRequired =
+    [ TestLabel "" $ TestCase $ assertEqual "" 4 (jumpsRequired g1)
+    ]
+
+
+
 d06Tests :: [Test]
-d06Tests = testTotalOrbits
+d06Tests = testTotalOrbits ++ testPathFromCenter ++ testJumpsRequired
